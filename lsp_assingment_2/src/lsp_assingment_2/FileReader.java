953d86ce234591dc1b3cc06368a5eee22c7520b6
Your Name <you@example.com>
2023-02-06 19:58:03 -0500

    hw2

---
+++ lsp_assingment_2/src/lsp_assingment_2/FileReader.java
@@ -0,0 +1,29 @@
+package lsp_assingment_2;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.net.URL;
+
+public class FileReader {
+	public String readToString(String resource) throws FileNotFoundException {
+		URL url = getClass().getClassLoader().getResource(resource);
+		
+		if (url != null) {
+			File file = new File(url.getPath());
+			
+			Scanner sc = null;
+			try {
+				sc = new Scanner(file);
+				String output = "";
+				while (sc.hasNextLine()) {
+					output = output + sc.nextLine() + " ";
+				}
+				return output.trim();
+			} finally {
+				sc.close();
+			}
+		}
+		throw new FileNotFoundException();
+	}
+
+}
